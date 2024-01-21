abf244c13ea427004b5e43bce589c3f8104a6177
jaehwanp <rkgkfk54@gmail.com>
2024-01-21 16:03:24 +0900

    node study

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate')
 
 ///////////////////////////////
 // FILES
@@ -30,20 +31,7 @@ const url = require('url')
 
 //////////////////////////////
 // SERVER
-const replaceTempate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g        , product.image)
-  output = output.replace(/{%PRICE%}/g        , product.price)
-  output = output.replace(/{%FROM%}/g         , product.from)
-  output = output.replace(/{%NUTRIENTS%}/g    , product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g     , product.quantity)
-  output = output.replace(/{%DESCRIPTION%}/g  , product.description)
-  output = output.replace(/{%ID%}/g           , product.id)
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-
-  return output
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard     = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
@@ -61,7 +49,7 @@ const server = http.createServer((req, res) => {
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' })
 
-    const cardsHtml = dataObj.map(el => replaceTempate(tempCard, el)).join('')
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
     res.end(output)
 
@@ -69,7 +57,7 @@ const server = http.createServer((req, res) => {
   } else if (pathname === '/product') {
     res.writeHead(200, { 'Content-type': 'text/html' })
     const product = dataObj[query.id]
-    const output = replaceTempate(tempProduct, product)
+    const output = replaceTemplate(tempProduct, product)
     res.end(output)
 
     // API
